08e554da3976981b14cad2bc58227e34129a4116
Jordan Powers <jordanpowers1227@gmail.com>
2026-02-12 21:18:51 -0800

    Add 3-pref overload for multiSubscribe

--- prefs/MoPrefsUtils.java
+++ prefs/MoPrefsUtils.java
@@ -6,6 +6,11 @@ import java.util.function.BiConsumer;
 
 public class MoPrefsUtils {
 
+    @FunctionalInterface
+    public interface TriConsumer<T, U, V> {
+        public void accept(T t, U u, V v);
+    }
+
     public static <T, U> void multiSubscribe(Pref<T> pref1, Pref<U> pref2, BiConsumer<T, U> subscription) {
         multiSubscribe(pref1, pref2, subscription, false);
     }
@@ -16,6 +21,21 @@ public class MoPrefsUtils {
         pref2.subscribe(value2 -> subscription.accept(pref1.get(), value2), false);
         if (notifyImmediately) {
             subscription.accept(pref1.get(), pref2.get());
+        }
+    }
+
+    public static <T, U, V> void multiSubscribe(
+            Pref<T> pref1, Pref<U> pref2, Pref<V> pref3, TriConsumer<T, U, V> subscription) {
+        multiSubscribe(pref1, pref2, pref3, subscription, false);
+    }
+
+    public static <T, U, V> void multiSubscribe(
+            Pref<T> pref1, Pref<U> pref2, Pref<V> pref3, TriConsumer<T, U, V> subscription, boolean notifyImmediately) {
+        pref1.subscribe(value1 -> subscription.accept(value1, pref2.get(), pref3.get()), false);
+        pref2.subscribe(value2 -> subscription.accept(pref1.get(), value2, pref3.get()), false);
+        pref3.subscribe(value3 -> subscription.accept(pref1.get(), pref2.get(), value3), false);
+        if (notifyImmediately) {
+            subscription.accept(pref1.get(), pref2.get(), pref3.get());
         }
     }
 
@@ -33,6 +53,28 @@ public class MoPrefsUtils {
         pref2.subscribe(value2 -> subscription.accept(pref1.get(), value2), false);
         if (notifyImmediately) {
             subscription.accept(pref1.get(), pref2.get());
+        }
+    }
+
+    public static <T extends Unit, U extends Unit, V extends Unit> void multiSubscribe(
+            UnitPref<T> pref1,
+            UnitPref<U> pref2,
+            UnitPref<V> pref3,
+            TriConsumer<Measure<T>, Measure<U>, Measure<V>> subscription) {
+        multiSubscribe(pref1, pref2, pref3, subscription, false);
+    }
+
+    public static <T extends Unit, U extends Unit, V extends Unit> void multiSubscribe(
+            UnitPref<T> pref1,
+            UnitPref<U> pref2,
+            UnitPref<V> pref3,
+            TriConsumer<Measure<T>, Measure<U>, Measure<V>> subscription,
+            boolean notifyImmediately) {
+        pref1.subscribe(value1 -> subscription.accept(value1, pref2.get(), pref3.get()), false);
+        pref2.subscribe(value2 -> subscription.accept(pref1.get(), value2, pref3.get()), false);
+        pref3.subscribe(value3 -> subscription.accept(pref1.get(), pref2.get(), value3), false);
+        if (notifyImmediately) {
+            subscription.accept(pref1.get(), pref2.get(), pref3.get());
         }
     }
 
